acecabb0c99cd7a3c47ddf639fc3a14faabf57bc
andreradtke <andreradtke@users.noreply.github.com>
2019-08-10 18:22:55 -0300

    Minor changes to variables

--- plot1.R
+++ plot1.R
@@ -2,14 +2,14 @@
 dataset <- read.csv2('household_power_consumption.txt', stringsAsFactors = FALSE, na.strings = '?')
 
 # Select the desired variable for histogram
-dataset <- subset(dataset, Date == '1/2/2007' | Date == '2/2/2007')
-globalActivePower <- dataset$Global_active_power
-globalActivePower <- as.numeric(unlist(globalActivePower))
+dataSubset <- subset(dataset, Date == '1/2/2007' | Date == '2/2/2007')
+dataSubset <- dataSubset$Global_active_power
+dataSubset <- as.numeric(unlist(dataSubset))
 
 # Memory cleanup
 rm(dataset)
 
 # Write histogram to png file
 png('plot1.png', width = 840, height = 840)
-hist(globalActivePower, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kWh)', ylab = 'Frequency', cex.lab = 1.3, cex.axis = 1.1, cex.main = 1.5)
+hist(dataSubset, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kW)', ylab = 'Frequency', cex.lab = 1.3, cex.axis = 1.1, cex.main = 1.5)
 dev.off()
